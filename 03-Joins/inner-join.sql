92116718ae9c7989e9108aa14575bc972dbd627f
Mateus Toré <mateus.tore@hotmail.com>
2026-08-18 17:55:26 -0300

    Create inner-join.sql

---
+++ 03-Joins/inner-join.sql
@@ -0,0 +1,42 @@
+SELECT
+    A.BusinessEntityID,
+    A.FirstName,
+    A.LastName,
+    B.EmailAddress
+FROM Person.Person AS A
+INNER JOIN Person.EmailAddress AS B
+    ON A.BusinessEntityID = B.BusinessEntityID;
+
+-------------------------------------------------------
+
+SELECT
+    A.ProductID,
+    A.Name AS ProductName,
+    A.ListPrice,
+    B.Name AS SubcategoryName
+FROM Production.Product AS A
+INNER JOIN Production.ProductSubcategory AS B
+    ON A.ProductSubcategoryID = B.ProductSubcategoryID;
+
+-------------------------------------------------------
+
+SELECT
+    A.PhoneNumberTypeID,
+    A.Name AS PhoneType,
+    B.BusinessEntityID,
+    B.PhoneNumber
+FROM Person.PhoneNumberType AS A
+INNER JOIN Person.PersonPhone AS B
+    ON A.PhoneNumberTypeID = B.PhoneNumberTypeID;
+
+-------------------------------------------------------
+
+SELECT
+    A.StateProvinceID,
+    A.Name AS StateProvince,
+    B.AddressID,
+    B.City,
+    B.AddressLine1
+FROM Person.StateProvince AS A
+INNER JOIN Person.Address AS B
+    ON A.StateProvinceID = B.StateProvinceID;
